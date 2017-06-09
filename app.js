ddefa5281d4974d9e2ec68413edff62d2e497cc7
Henry Otighe <oti4me@gmail.com>
2017-06-10 00:22:22 +0100

    Added front end routing with html css and javascritp using ejs template engine

--- app.js
+++ app.js
@@ -4,21 +4,26 @@ var	http = require('http');
 var	path = require('path');
 
 var	config = require('./config/config');
+var	routesHandler = require('./routes/main');
+var	usersHandler = require('./routes/users');
+var	adminHandler = require('./routes/admin');
+var	partnersHandler = require('./routes/partners');
 
 //instantiating express
 var app = express();
 
 //View engine configuration
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 //Mounting express middlewere
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes and routes handlers
-app.get('/', function(req, res){
-	res.end("Home Page");
-});
+app.use('/users', usersHandler);
+app.use('/admin', adminHandler);
+//app.use('/partners', partnersHandler);
+app.use('/', routesHandler);
 
 //Starting server and listening on the server port or 3000 on a local environment.
 http.createServer(app).listen(config.PORT, function() {
